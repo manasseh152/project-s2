45ba64cf9d412c8ee893f819633719c1e7d39047
manasseh152 <manasseh2424@gmail.com>
2023-01-23 13:39:57 +0100

    Init

---
+++ app/views/homepages/index.php
@@ -0,0 +1,5 @@
+<h3><?= $data["title"]; ?></h3>
+<a href="<?= URLROOT; ?>/countries/index">Landenoverzicht</a> <br>
+<a href="<?= URLROOT; ?>/RichestPeoples/index">Rijkste mensen</a><br>
+<a href="<?= URLROOT; ?>/Lessen/index">Overzicht rijlessen</a><br>
+<a href="<?= URLROOT; ?>/wagenparken/index"> Overzicht Wagenpark</a>
