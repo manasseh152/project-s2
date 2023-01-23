c4d9ee44960922d2de0b71b3d21dd0226336c0e9
manasseh152 <manasseh2424@gmail.com>
2023-01-23 22:53:59 +0100

    Added example config.php

---
+++ app/config/config.example.php
@@ -0,0 +1,11 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'BE-Oefentoets-MVC');
+
+define('APPROOT', dirname(dirname(__FILE__)));
+
+define('URLROOT', 'http://local.project.gg');
+
+define('SITENAME', 'MVC Framework oefenToets');
